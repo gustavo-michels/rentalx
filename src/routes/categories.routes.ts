3c320e30f5ba483f8441e5dbf6e0f2011b2a2684
Gustavo H. Michels <ghmichels@gmail.com>
2022-07-20 18:22:36 -0300

    Refactoring categories list

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,14 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 export const categoriesRoute = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoute.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoute.get("/", (request, response) => {
-  const allCategories = categoriesRepository.list();
-  return response.json(allCategories);
+  return listCategoriesController.handle(request, response);
 });
