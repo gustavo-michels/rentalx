aec7ce414d8d5cb8b5ab5882c3323391f8b1f12e
Gustavo H. Michels <ghmichels@gmail.com>
2022-07-21 10:51:08 -0300

    Added feature category import with multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 export const categoriesRoute = Router();
+
+const upload = multer({ dest: "./tmp" });
 
 categoriesRoute.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,4 +15,8 @@ categoriesRoute.post("/", (request, response) => {
 
 categoriesRoute.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoute.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
